d813f44ea05bd75e19fc485cd37e9e3605098683
tomas <totobarajas124@gmail.com>
2025-06-06 12:22:01 -0300

    add mail to in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,20 +1,20 @@
-import Link from 'next/link'
-import Logo from './Logo'
-import Cell from './icons/Cell'
-import Buzon from './icons/Buzon'
-import Home from './icons/Home'
+import Link from "next/link";
+import Logo from "./Logo";
+import Cell from "./icons/Cell";
+import Buzon from "./icons/Buzon";
+import Home from "./icons/Home";
 // import Persona1 from './icons/Persona1'
-import Persona2 from './icons/Persona2'
-import Contact from './icons/Contact'
-import { moonDance } from '../layout'
+import Persona2 from "./icons/Persona2";
+import Contact from "./icons/Contact";
+import { moonDance } from "../layout";
 
 export default function Footer() {
   return (
-    <footer className='absolute z-0 flex flex-col flex-wrap items-center justify-between w-full text-black bg-white gap-y-5'>
-      <ul className='flex flex-wrap items-center justify-between w-full px-16 py-5 text-white underline gap-y-5 bg-tertiary'>
+    <footer className="absolute z-0 flex flex-col flex-wrap items-center justify-between w-full text-black bg-white gap-y-5">
+      <ul className="flex flex-wrap items-center justify-between w-full px-16 py-5 text-white underline gap-y-5 bg-tertiary">
         <Link href="/documentation">
           <li>● Documentación para salir del país</li>
-        </Link>      
+        </Link>
         <Link href="/cancelation">
           <li>● Política de cancelación</li>
         </Link>
@@ -28,50 +28,71 @@ export default function Footer() {
           <li>● Cartelera</li>
         </Link>
       </ul>
-      <section className='flex flex-col justify-between w-full gap-10 px-20 md:gap-0 md:flex-row'>
-        <div className='flex flex-col gap-2'>
-            <Logo w="200px" />
-            <img src="/ministerio.png" alt="Logo"/>
+      <section className="flex flex-col justify-between w-full gap-10 px-20 md:gap-0 md:flex-row">
+        <div className="flex flex-col gap-2">
+          <Logo w="200px" />
+          <img src="/ministerio.png" alt="Logo" />
         </div>
-        <ul className='flex flex-col justify-between gap-5 md:gap-0'>
-            <p className='text-xl font-bold'>
-              Contacto</p>
-            <li className='flex items-center gap-5 text-sm md:text-normal'>
-              <Cell />
-              
-              11-6521-6518</li>
-            <li className='flex items-center gap-5 text-sm md:text-normal'>
+        <ul className="flex flex-col justify-between gap-5 md:gap-0">
+          <p className="text-xl font-bold">Contacto</p>
+          <li className="flex items-center gap-5 text-sm md:text-normal">
+            <Cell />
+            11-6521-6518
+          </li>
+          <li className="flex items-center gap-5 text-sm md:text-normal">
+            <Link href="mailto:reservas@estelarviajes.com">
               <Buzon />
-              
-              reservas@estelarviajes.com</li>
-            <li className='flex items-center gap-5 text-sm md:text-normal'>
+            </Link>
+            reservas@estelarviajes.com
+          </li>
+          <li className="flex items-center gap-5 text-sm md:text-normal">
+            <Link href="mailto:info@estelarviajes.com">
               <Buzon />
-              
-              info@estelarviajes.com</li>
+            </Link>
+            info@estelarviajes.com
+          </li>
         </ul>
-        <ul className='flex flex-col justify-between gap-5 md:gap-0'>
-            <p className='text-xl font-bold'>Mapa del sitio</p>
-            <Link className='flex items-center gap-5 text-sm underline md:text-medium' href="/">
-            <Home fill={"black"}/>
-            Inicio</Link>
-            {/* <Link className='flex items-center gap-5 text-sm underline md:text-medium' href="/travel-packages">
+        <ul className="flex flex-col justify-between gap-5 md:gap-0">
+          <p className="text-xl font-bold">Mapa del sitio</p>
+          <Link
+            className="flex items-center gap-5 text-sm underline md:text-medium"
+            href="/"
+          >
+            <Home fill={"black"} />
+            Inicio
+          </Link>
+          {/* <Link className='flex items-center gap-5 text-sm underline md:text-medium' href="/travel-packages">
             <Persona1 fill={"black"} />
             Paquetes</Link> */}
-            <Link className='flex items-center gap-5 text-sm underline md:text-medium' href="/about-us">
+          <Link
+            className="flex items-center gap-5 text-sm underline md:text-medium"
+            href="/about-us"
+          >
             <Persona2 fill={"black"} />
-            ¿Quienes Somos?</Link>
-            <Link className='flex items-center gap-5 text-sm underline md:text-medium' href="/contact">
+            ¿Quienes Somos?
+          </Link>
+          <Link
+            className="flex items-center gap-5 text-sm underline md:text-medium"
+            href="/contact"
+          >
             <Contact fill={"black"} />
-            Contacto</Link>
+            Contacto
+          </Link>
         </ul>
       </section>
       <div className="flex flex-col items-center justify-center gap-5 mt-5">
-                   <p className={`md:text-xl ${moonDance.className}`}>Razon social: MUNDO TERRAMAR S.R.L - Cuit: 30-71637562-1 </p> 
-                   <Link href="https://iwebtecnology.com" target="_blank" className="flex items-center justify-center">
-                      <p>Desarrollado por</p>
-                      <img src="/iweb.png" alt="Logo" width="121"/>
-                    </Link>
+        <p className={`md:text-xl ${moonDance.className}`}>
+          Razon social: MUNDO TERRAMAR S.R.L - Cuit: 30-71637562-1{" "}
+        </p>
+        <Link
+          href="https://iwebtecnology.com"
+          target="_blank"
+          className="flex items-center justify-center"
+        >
+          <p>Desarrollado por</p>
+          <img src="/iweb.png" alt="Logo" width="121" />
+        </Link>
       </div>
     </footer>
-  )
+  );
 }
